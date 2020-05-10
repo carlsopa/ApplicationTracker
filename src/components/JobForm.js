6fdb753492e270accb2ec47e678e96a5270b9db9
Paul R. Carlson <carlsonpar@gmail.com>
2020-05-09 20:49:08 -0400

    updated the form so that it will act as a modal

--- src/components/JobForm.js
+++ src/components/JobForm.js
@@ -19,44 +19,40 @@ const JobForm = (props) => {
     <div className="modal">
       <div className="modal-content">
         <form id="JobForm">
-          <label>
-            Company Name
+          <label for="company">
+            Company Name</label>
             <input
               type="text"
               id="Company"
               onChange={(e) => setCompany(e.target.value)}
             ></input>
-          </label>
-          <label>
-            Job title
+          <label for="Title">
+            Job title</label>
             <input
               type="text"
               id="Title"
               onChange={(e) => setTitle(e.target.value)}
             ></input>
-          </label>
-          <label>
-            Category
+          <label for="Category">
+            Category</label>
             <select id="Category" onChange={(e) => setCategory(e.target.value)}>
               <option value="Front End Developer">Front End Developer</option>
               <option value="Back End Developer">Back End Developer</option>
               <option value="Non-Developer">Non-Developer</option>
             </select>
-          </label>
-          <label id="Description">
-            Notes
-            <textarea
+          <label id="DescriptionLabel" for="Description">
+            Notes</label>
+            <textarea id="Description"
               onChange={(e) => setDescription(e.target.value)}
             ></textarea>
-          </label>
-          <label>
-            Job link
+
+          <label for="Link">
+            Job link</label>
             <input
               type="text"
               id="Link"
               onChange={(e) => setUrl(e.target.value)}
             ></input>
-          </label>
           <input
             id="SubmitJob"
             type="submit"
