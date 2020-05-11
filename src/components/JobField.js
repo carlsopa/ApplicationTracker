3b9c958587cc666be1923d4a7c79595af1325c9c
Paul R. Carlson <carlsonpar@gmail.com>
2020-05-11 19:35:08 -0400

    updated the colors on the cards to reflect the current status of the job

--- src/components/JobField.js
+++ src/components/JobField.js
@@ -8,7 +8,7 @@ const JobField = (props) => {
   };
   return (
 
-      <div className="PreviewCard" onClick={(e) => ClickEvent(e)}>
+      <div className={props.data["status"]==="Declined"?"PreviewCard declined":props.data["status"]!=="Applied"?"PreviewCard processing":"PreviewCard"} onClick={(e) => ClickEvent(e)}>
         <div className="company" id="RowCompany">{props.data["company"]}</div>
         <div id="RowStatus">{props.data["status"]}</div>
         <div className="update" id="RowUpdated">{props.data["update"]}</div>
