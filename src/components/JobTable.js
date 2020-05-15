8e2a0ad4d0f9102b0c1419c3c07afa086bb792ee
Paul R. Carlson <carlsonpar@gmail.com>
2020-05-15 18:26:33 -0400

    added new feature to allow for the sorting of jobs

--- src/components/JobTable.js
+++ src/components/JobTable.js
@@ -1,21 +1,102 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import JobField from "./JobField";
 
 const JobTable = (props) => {
+  const [Category, setCategory] = useState("all");
+  const [Status, setStatus] = useState("all");
+  const [SortedData, setSortedData] = useState([]);
+  const [SortedBoolean, setSortedBoolean] = useState(false);
+
+  useEffect(() => {
+    console.log("effect")
+    var data = [];
+    
+    props.application.map((Applications, index) => {
+      if (Category != "all") {
+        setSortedBoolean(true);
+        if (Status != "all") {
+          if (
+            Applications.category == Category &&
+            Applications.status == Status
+          ) {
+            data.push(Applications);
+          }
+        } else {
+          if (Applications.category == Category) {
+            data.push(Applications);
+          }
+        }
+      } else {
+        if (Applications.status == Status) {
+          setSortedBoolean(true);
+          data.push(Applications);
+        }
+        
+      }
+      if(Category == "all" && Status=="all"){
+        setSortedBoolean(false)
+      }
+      setSortedData(data);
+
+    });
+  }, [Status, Category]);
+  
+  
   return (
-    <div className="JobTable">
-      {props.application
-        ? props.application.map((Data, index) => (
-            <JobField
-              setBoolean={props.click}
-              data={Data}
-              index={index}
-              click={props.idSet}
-              id={index}
-              delete={props.delete}
-            />
-          ))
-        : null}
+    <div id="SortForm">
+      <label for="CategorySort">Sort by category: </label>
+      <select id="CategorySort" onChange={(e) => setCategory(e.target.value)}>
+        <option value="all">all</option>
+        <option value="Front End Developer">Front-End Developer</option>
+        <option value="Back End Developer">Back-End Developer</option>
+        <option value="Non-Developer">Other</option>
+      </select>
+      <label for="StatusSort">Sort by application status: </label>
+      <select id="StatusSort" onChange={(e) => setStatus(e.target.value)}>
+        <option value="all">All</option>
+        <option value="Applied">Applied</option>
+        <option value="Phone interview">Phone interview</option>
+        <option value="In-person">In-person interview</option>
+        <option value="Declined">Rejected</option>
+      </select>
+      <div className="JobTable">
+        {SortedBoolean
+        ? SortedData.map((Data,index)=>(
+          <JobField
+                setBoolean={props.click}
+                data={Data}
+                index={index}
+                click={props.idSet}
+                id={index}
+                delete={props.delete}
+              />
+        )):props.application
+          ? props.application.map((Data, index) => (
+              <JobField
+                setBoolean={props.click}
+                data={Data}
+                index={index}
+                click={props.idSet}
+                id={index}
+                delete={props.delete}
+              />
+            ))
+          : null}
+        {
+        // props.application
+        //   ? props.application.map((Data, index) => (
+        //       <JobField
+        //         setBoolean={props.click}
+        //         data={Data}
+        //         index={index}
+        //         click={props.idSet}
+        //         id={index}
+        //         delete={props.delete}
+        //       />
+        //     ))
+        //   : null
+          }
+      </div>
     </div>
   );
 };
